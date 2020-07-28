5a48c31180f7e311a50d6ad17110818676d0f6ea
f-westergren <folke.westergren@gmail.com>
2020-07-28 11:44:27 -0400

    add some seed data

--- data.sql
+++ data.sql
@@ -6,4 +6,12 @@ CREATE TABLE companies (
   num_employees INTEGER,
   description TEXT,
   logo_url TEXT
-)
+);
+
+INSERT INTO companies 
+  (handle, name, num_employees, description) 
+VALUES 
+  ('ibm', 'IBM', 10000, 'Big computer company'),
+  ('dell', 'DELL', 5000, 'Another big computer company'),
+  ('mcd', 'McDonalds', 100000, 'Burger place'),
+  ('rose_rocket', 'Rose Rocket', 50, 'Logistics software company');
