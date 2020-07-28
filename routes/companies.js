af2c65926117f6b48abd7a0e7dbaeb984c8a1591
f-westergren <folke.westergren@gmail.com>
2020-07-28 12:06:37 -0400

    add companySchema

--- routes/companies.js
+++ routes/companies.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const ExpressError = require('../helpers/expressError');
-const jsonschema = require('jsonschema');
 const Company = require('../models/company')
 const router = new express.Router()
+const checkCompanySchema = require('../middleware/companySchema')
 
 /** GET /, returns name and handle for all company objects.
  * The following query strings are accepted:
@@ -25,7 +25,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', checkCompanySchema, async (req, res, next) => {
   try {
     const {handle, name, num_employees, description, logo_url } = req.body
     const company = await Company.add(handle, name, num_employees, description, logo_url)
@@ -46,9 +46,9 @@ router.get('/:handle', async (req, res, next) => {
   }
 })
 
-router.patch('/:handle', async (req, res, next) => {
+router.patch('/:handle', checkCompanySchema, async (req, res, next) => {
   try {
-    let company = await Company.findOne(req.params.handle, req.body)
+    let company = await Company.update(req.params.handle, req.body)
 
     return res.send(company)
   } catch (error) {
