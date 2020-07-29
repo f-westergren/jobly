6a7ae948a6858a623725781db2f9b15225ec7c07
f-westergren <folke.westergren@gmail.com>
2020-07-29 14:46:22 -0400

    add job tests

---
+++ __tests__/integration/jobs.test.js
@@ -0,0 +1,163 @@
+const request = require('supertest');
+const app = require('../../app');
+const db = require('../../db')
+
+beforeAll(async () => {
+  await db.query(
+    `INSERT INTO companies 
+      (handle, name)
+    VALUES 
+      ('test', 'Test Company'),
+      ('test2', 'Test Company 2')
+  `)
+})
+
+beforeEach(async () => {
+  let result = await db.query(
+    `INSERT INTO jobs 
+      (title, salary, equity, company_handle)
+    VALUES 
+      ('Tester 1', '25000', 0, 'test'),
+      ('Tester 2', '50000', 0.25, 'test2'),
+      ('Tester 3', '100000', 0.50, 'test')
+    RETURNING *`
+  )
+  
+  // Testcompanies with just names and handles defined. 
+  testJob1 = { title: result.rows[0].title, company_handle: result.rows[0].company_handle }
+  testJob2 = { title: result.rows[1].title, company_handle: result.rows[1].company_handle }
+  testJob3 = { title: result.rows[2].title, company_handle: result.rows[2].company_handle }
+  
+  // Testcompany with all details defined.
+  testJob4 = result.rows[0]
+})
+
+afterEach(async () => {
+  await db.query(`DELETE FROM jobs`)
+})
+
+afterAll(async () => {
+  await db.query(`DELETE FROM companies`)
+  await db.end()
+})
+
+describe('GET /jobs', () => {
+  test('get a list of all companies, no search parameters', async () => {
+    const res = await request(app).get('/jobs')
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ jobs: [testJob1, testJob2, testJob3] })
+  })
+
+  test('get a list of jobs with min_salary and min_equity parameters', async () => {
+    const res = await request(app).get('/jobs').send({min_salary: 26000, min_equity: 0.4})
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ jobs: [testJob3] })
+  })
+
+  test('get a list of jobs mith search string', async () => {
+    const res = await request(app).get('/jobs').send({search: 'test2'})
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ jobs: [testJob2] })
+  })
+
+  test('get an empty list of jobs mith search string', async () => {
+    const res = await request(app).get('/jobs').send({search: 'NoJob'})
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ jobs: [] })
+  })
+})
+
+describe('POST /jobs', () => {
+  test('add a new job', async () => {
+    const jobTest = {
+      title: 'QA Tester', 
+      salary: 99999, 
+      equity: 0.99,
+      company_handle: 'test'
+    }
+    const res = await request(app).post('/jobs').send(jobTest)
+
+    // Set id and date_posted
+    jobTest.id = res.body.job.id
+    jobTest.date_posted = res.body.job.date_posted
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ job: jobTest })
+
+    // Make sure job was created
+    const getJobRes = await request(app).get(`/jobs/${jobTest.id}`)
+    expect(getJobRes.body.job).toEqual(jobTest)
+  })
+})
+  test('returns 400 with invalid input', async () => {
+    const res = await request(app).post('/jobs').send({
+      title: '123', 
+      salary: 'high', 
+      equity: 'please', 
+      company_handle: 'test'
+    })
+    expect(res.statusCode).toBe(400)
+  })
+
+describe('GET /jobs/:id', () => {
+  test('gets a single job', async () => {
+    const res = await request(app).get(`/jobs/${testJob4.id}`)
+    expect(res.statusCode).toBe(200)
+    expect(res.body.job).toHaveProperty('id', testJob4.id)
+  })
+  test('returns 404 for invalid id ', async () => {
+    const res = await request(app).get('/jobs/0')
+    expect(res.statusCode).toBe(404)
+    expect(res.body.message).toEqual("Couldn't find job with id 0")
+  })
+})
+
+describe('PATCH /jobs/:id', () => {
+  test('updates a single job', async () => {
+    // Update testJob4 data with this:
+    const updateData = {
+      title: 'A new job title', 
+      salary: 500, 
+      equity: 0.33,
+      company_handle: 'test' 
+    }
+
+    // Send update request
+    const res = await request(app).patch(`/jobs/${testJob4.id}`).send(updateData)
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ job: updateData })
+
+    // Check updated company data
+    const getJobRes = await request(app).get(`/jobs/${testJob4.id}`)
+    expect(getJobRes.body.job).toHaveProperty('title', 'A new job title')
+  })
+
+  test('returns 404 for invalid id', async () => {
+    const res = await request(app).patch('/jobs/0').send({
+      title: 'A title', 
+      salary: 230, 
+      equity: 0, 
+      company_handle: 'test'
+    })
+
+    expect(res.statusCode).toBe(404)
+    expect(res.body.message).toEqual("Couldn't find job with id 0")
+  })
+})
+
+describe('DELETE /jobs/:id', () => {
+  test('Deletes a single job', async () => {
+    const res = await request(app).delete(`/jobs/${testJob4.id}`)
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ message: 'Job deleted' })
+
+    // Make sure company is deleted:
+    const getJobRes = await request(app).get(`/jobs/${testJob4.id}`)
+    expect(getJobRes.statusCode).toEqual(404)
+  })
+  test('responds with 404 for invalid id', async () => {
+    const res = await request(app).delete('/jobs/0')
+    expect(res.statusCode).toBe(404)
+    expect(res.body.message).toEqual("Couldn't find job with id 0")
+  })
+})
+  
