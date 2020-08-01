28dfc8c50148f0e2e09723a35a3d819ea4cbfb7f
f-westergren <folke.westergren@gmail.com>
2020-08-01 15:56:23 -0400

    add test work factor for bcrypt

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "test";
 
-const PORT = +process.env.PORT || 3000;
+const PORT = +process.env.PORT || 4000;
 
 // database is:
 //
@@ -13,15 +13,19 @@ const PORT = +process.env.PORT || 3000;
 // - else: 'jobly'
 
 let DB_URI;
+let BCRYPT_WORK_FACTOR;
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = "jobly-test";
+  BCRYPT_WORK_FACTOR = 1
 } else {
   DB_URI = process.env.DATABASE_URL || "jobly";
+  BCRYPT_WORK_FACTOR = 12
 }
 
 module.exports = {
   SECRET_KEY,
   PORT,
-  DB_URI
+  DB_URI,
+  BCRYPT_WORK_FACTOR
 };
